148aed35d01a0e001cd7b74322e26e8f3b1529a4
Bruno Cesar <brunocesar902@gmail.com>
2022-10-08 12:47:41 -0300

    Segundo Commit - Exemplos de como usar o reverse.

---
+++ reverse.js
@@ -0,0 +1,13 @@
+// reverse() inverte a ordem dos itens de um array.
+
+//Exemplo 01:
+const frutas = ['Pera', 'Uva', 'Kiwi', 'Laranja'];
+
+let ordemAlterada = frutas.reverse(); // Inversão dos itens do array frutas.
+console.log(ordemAlterada); // Imprime os valores invertidos.
+
+//Exemplo 02:
+const nome = ['Cesar','Bruno'];
+
+let nomeInvertido = nome.reverse();
+console.log(nomeInvertido); // Mostra Bruno Cesar
