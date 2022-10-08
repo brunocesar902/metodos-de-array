9ac67451e1cf63c26518cd0bf1176699a26cabeb
Bruno Cesar <brunocesar902@gmail.com>
2022-10-08 11:28:10 -0300

    Primeiro Commit

---
+++ indexOf.js
@@ -0,0 +1,9 @@
+const nomes = ['Bruno', 'Kátia', 'Bene', 'Gali'];
+
+let indice = nomes.indexOf('Bene'); // Retorna 2 
+console.log(indice);
+
+// indexOf retorna o indice do item procurado no array.
+
+const nome = 'Bruno Cesar';
+console.log(nome.indexOf('e')); // Retorna 7
