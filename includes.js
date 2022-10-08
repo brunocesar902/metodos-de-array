7ce9e1920e6c12cee7ca92c37db2cf023d548a63
Bruno Cesar <brunocesar902@gmail.com>
2022-10-08 11:42:32 -0300

    Segundo Commit - Exemplo de como usar o includes.

---
+++ includes.js
@@ -0,0 +1,9 @@
+const nomes = ['Bruno', 'Kátia', 'Bene', 'Gali'];
+
+let boolean = nomes.includes('Bene'); // Retorna um booleano
+console.log(boolean); // Imprime true.
+
+// includes retorna um valor booleano..
+
+const nome = 'Bruno Cesar';
+console.log(nome.includes('e')); // Retorna true
