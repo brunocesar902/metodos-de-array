4a9fd20e6f5f5a5b14f07b67598e03f1b28cafa9
Bruno Cesar <brunocesar902@gmail.com>
2022-10-10 10:18:18 -0300

    Segundo Commit - Exemplos de como usar o concat.

---
+++ concat.js
@@ -0,0 +1,8 @@
+const primeiroNome = ['Bruno'];
+const segundoNnome = ['Cesar'];
+const terceiroNome = ['Barbosa'];
+const quartoNome = ['Lemos'];
+
+// Concatenando os arrays com os nomes, para imprimir o nome completo.
+let nomeCompleto = primeiroNome.concat(segundoNnome, terceiroNome, quartoNome);
+console.log(nomeCompleto);
